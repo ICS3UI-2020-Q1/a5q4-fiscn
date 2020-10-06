344ba515a360ef41de72daa9b771ec7a46925523
fiscn <fiscn2709@wrdsb.ca>
2020-10-06 15:47:13 +0000

    Finished A5 Q4 program

--- Main.java
+++ Main.java
@@ -1,6 +1,8 @@
+import java.util.Scanner; 
+
 /**
- *
- * @author 
+ * calculate intrest rate for number inputed by user
+ * @author Neil Fischer
  */
 public class Main {
 
@@ -9,7 +11,29 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    //Create scanner for user input 
+    Scanner input = new Scanner(System.in); 
+
+    //prompt user to enter in starting balance and create variable for balance and total at end
+    System.out.println("Please enter starting balance"); 
+    double balanceN = input.nextDouble(); 
+    double totalN = 1;
+
+    //prompt the user to enter they're intrest rate and create variable for intrest rate 
+    System.out.println("Please enter intrest rate"); 
+    double intrestN = input.nextDouble(); 
+    intrestN = intrestN / 100; 
+
+    //prompt the user to enter the number of years they will be investing and create variable for years 
+    System.out.println("Please enter how many years you will be investing"); 
+    int yearsN = input.nextInt(); 
+
+    //create loop to calculate their intrest after the years typed by user 
+    for(double count = 1; count <= yearsN; count++){
+      balanceN = balanceN * intrestN + balanceN;   
+    } 
     
+    //create line for total
+     System.out.print("Your final balance would be " + balanceN);
   }
 }
